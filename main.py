03b1f006028237d7ae6c9ba5951a9c20e76ddebc
ChengFu Lin <chengfulin0806@gmail.com>
2017-05-29 16:44:41 +0800

    Add label_image

--- main.py
+++ main.py
@@ -8,13 +8,6 @@ import label_image
 app = Flask(__name__)
 
 
-@app.route('/api/mnist', methods=['POST'])
-def mnist():
-    input = ((255 - np.array(request.json, dtype=np.uint8)) / 255.0).reshape(1, 784)
-    output1 = regression(input)
-    output2 = convolutional(input)
-    return jsonify(results=[output1, output2])
-
 @app.route('/api/trydetect/<img>')
 def tryDetect(img):
     return detect(img)
